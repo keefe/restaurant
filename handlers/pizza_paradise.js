986fd22ea6b41adf7900d859d82e6ce4b1bd6c00
katie <katie@categorize.us>
2016-03-31 17:44:26 -0700

    ass hole keefe

---
+++ handlers/pizza_paradise.js
@@ -0,0 +1,37 @@
+var AWS = require('aws-sdk');
+AWS.config.update({region:'us-west-2'});
+
+module.exports = function(event, context) {
+    var newItem = {
+        Item:{
+          email:{
+              S:"keefe@categorize.us"
+          },
+          firstName:{
+              S:"keefe"
+          },
+          lastName:{
+              S:"roedersheimer"
+          },
+          userMessage:{
+              S:"Test Message"
+          }
+        },
+        TableName:'pizza_paradise'
+    };
+    if(event.firstName!==undefined && event.firstName !=='') newItem.Item.firstName.S = event.firstName;
+    if(event.lastName!==undefined && event.lastName !=='') newItem.Item.lastName.S = event.lastName;
+    if(event.email!==undefined && event.email!=='') newItem.Item.email.S = event.email;
+    if(event.userMessage!==undefined && event.userMessage!=='') newItem.Item.userMessage.S = event.userMessage;
+    
+    var dynamo = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+    dynamo.putItem(newItem, function(err, data){
+       if(err){
+           console.log(err, err.stack);
+           context.fail(err);
+       }else{
+            console.log(data);
+            context.succeed(data);
+       }
+    });
+};
