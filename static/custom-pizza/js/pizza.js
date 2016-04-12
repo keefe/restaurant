4931fa7906fe5a9b3788fcc240b9e8ad4098d47b
Katie Roedersheimer <katie@categorize.us>
2016-04-11 17:35:27 -0700

    adding custom-pizza form, changed jquery to CDN

---
+++ static/custom-pizza/js/pizza.js
@@ -0,0 +1,114 @@
+     $('.pep').fadeToggle(1000);
+     $('.green-pepper').fadeToggle(1000);
+     $('.mushroom').fadeToggle(1000);
+     var totalPrice = 10;
+     var pepPrice = 1;
+     var mushPrice = 1;
+     var greenPrice = 1;
+     var saucePrice = 3;
+     var crustPrice = 5;
+     var order = {
+        pepperoni:false,
+        greenPeppers:false,
+        mushrooms:false,
+        glutenFree:false,
+        garlicSauce:false
+     };
+     $("#btnOrder").click(function(){
+        console.log("Send " + JSON.stringify(order) + " to the server");
+     });
+
+   	$('.btn-pepperonni').on('click', function(event) {
+        event.preventDefault();
+        order.pepperoni = !order.pepperoni;
+        $('.btn-pepperonni').toggleClass('active');
+        $('.pep').fadeToggle(1000);
+        $('strong').empty();
+        if ($('.btn-pepperonni').hasClass('active')){
+			totalPrice -= pepPrice; 
+   	        $('#pepperoni').remove();
+        }
+        else{
+   	        totalPrice += pepPrice;
+   	        $('#price-list').append('<li id="pepperoni"> Pepperoni: ' + '$' + pepPrice + '</li>');
+        }
+        $('strong').append("$");
+        $('strong').append(totalPrice);
+    });
+
+    $('.btn-green-peppers').on('click', function(event) {
+        event.preventDefault();
+        order.greenPeppers = !order.greenPeppers;
+        $('.btn-green-peppers').toggleClass('active');        
+        $('.green-pepper').fadeToggle(1000);
+        $('strong').empty();
+        if ($('.btn-green-peppers').hasClass('active')){
+			totalPrice -= greenPrice;
+   	        $('#green-pepper').remove();
+        }
+        else{
+   	        totalPrice += greenPrice;
+   	        $('#price-list').append('<li id="green-pepper"> Green Pepper: ' + '$' + greenPrice + '</li>');
+        }
+        $('strong').append("$");
+        $('strong').append(totalPrice);
+
+    });
+
+    $('.btn-mushrooms').on('click', function(event) {
+        event.preventDefault();
+        order.mushrooms = !order.mushrooms;
+
+        $('.btn-mushrooms').toggleClass('active');        
+        $('.mushroom').fadeToggle(1000);
+        $('strong').empty();
+        if ($('.btn-mushrooms').hasClass('active')){
+			totalPrice -= mushPrice;
+   	        $('#mushroom').remove();
+        }
+        else{
+   	        totalPrice += mushPrice;
+   	        $('#price-list').append('<li id="mushroom"> Mushroom: ' + '$' + mushPrice + '</li>');
+        }
+        $('strong').append("$");
+        $('strong').append(totalPrice);
+
+    });
+
+    $('.btn-crust').on('click', function(event) {
+        event.preventDefault();
+        order.glutenFree = !order.gluenFree;
+        $('.btn-crust').toggleClass('active');        
+        $('.crust').toggleClass('crust-gluten-free');
+        $('strong').empty();
+        if ($('.btn-crust').hasClass('active')){
+			totalPrice -= crustPrice;
+   	        $('#crust').remove();
+        }
+        else{
+   	        totalPrice += crustPrice;
+   	        $('#price-list').append('<li id="crust"> Crust: ' + '$' + crustPrice + '</li>');   	        
+        }
+        $('strong').append("$");
+        $('strong').append(totalPrice);
+
+    });
+
+     $('.btn-sauce').on('click', function(event) {
+        event.preventDefault();
+        order.garlicSauce = !order.garlicSauce;
+        $('.btn-sauce').toggleClass('active');
+        $('.sauce').toggleClass('sauce-white');
+        $('strong').empty();
+        if ($('.btn-sauce').hasClass('active')){
+			totalPrice -= saucePrice;
+   	        $('#sauce').remove();			
+        }
+        else{
+   	        totalPrice += saucePrice;
+   	        $('#price-list').append('<li id="sauce"> Garlic Sauce: ' + '$' + saucePrice  + '</li>');
+        }
+        $('strong').append("$");        
+        $('strong').append(totalPrice);
+    });
+
