d073b1cddbd3e42d3669f330a930b0510130dc71
keefe roedersheimer <keefe.roedersheimer@bluescape.com>
2016-04-17 12:43:49 -0700

    mitigate TLS 1.0 vs 1.2 issues resulting in intermittent NPROTO

--- handlers/contactlist.js
+++ handlers/contactlist.js
@@ -1,18 +1,28 @@
 var AWS = require('aws-sdk');
+var https = require('https');
 AWS.config.update({region:'us-west-2'});
 
 module.exports = function(event, context) {
-  var dynamo = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+  
+   var dynamo = new AWS.DynamoDB(
+  {
+     apiVersion: '2012-08-10',
+    httpOptions: {
+      agent: new https.Agent(
+      {
+        ciphers: 'ALL',
+        secureProtocol: 'TLSv1_method'
+      })
+    }
+  });
   var query = {
     TableName:'UserLeads'
   };
   dynamo.scan(query, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else {
-      var results = [];
-      for(var item of data.Items){
-        
-      }
+    if (err){
+      console.log(err, err.stack); // an error occurred
+    }else {
+      console.log("Query is complete");
       console.log(data);           // successful response
       context.done(null, data);
     }    
