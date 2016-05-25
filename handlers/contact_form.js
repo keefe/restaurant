755c9ceb4fddaf1d0a747800e258c6ce2813aaf9
Katie Roedersheimer <katie@categorize.us>
2016-05-25 13:46:04 -0700

    adding new updated contact_form.js with new json formating

--- handlers/contact_form.js
+++ handlers/contact_form.js
@@ -2,27 +2,21 @@ var AWS = require('aws-sdk');
 AWS.config.update({region:'us-west-2'});
 
 module.exports = function(event, context) {
-    var newItem = {
-        Item:{
-          email:{
-              S:"keefe@categorize.us"
-          },
-          firstName:{
-              S:"keefe"
-          },
-          lastName:{
-              S:"roedersheimer"
-          },
-          userMessage:{
-              S:"Test Message"
-          }
+    var newItem = {"": [
+        {"email": "keefe@categorize.us"},
+        {"firstName": "keefe"},
+        {"lastName": "roedershiemer"},
+        {"userMessage": "Test Message"}
+    ]
+};
+
         },
         TableName:'UserLeads'
     };
-    if(event.firstName!==undefined && event.firstName !=='') newItem.Item.firstName.S = event.firstName;
-    if(event.lastName!==undefined && event.lastName !=='') newItem.Item.lastName.S = event.lastName;
-    if(event.email!==undefined && event.email!=='') newItem.Item.email.S = event.email;
-    if(event.userMessage!==undefined && event.userMessage!=='') newItem.Item.userMessage.S = event.userMessage;
+    if(event.firstName!==undefined && event.firstName !=='') newItem.Item.firstName = event.firstName;
+    if(event.lastName!==undefined && event.lastName !=='') newItem.Item.lastName = event.lastName;
+    if(event.email!==undefined && event.email!=='') newItem.Item.email = event.email;
+    if(event.userMessage!==undefined && event.userMessage!=='') newItem.Item.userMessage = event.userMessage;
     
     var dynamo = new AWS.DynamoDB({apiVersion: '2012-08-10'});
     dynamo.putItem(newItem, function(err, data){
